16de736a470ac5317b27f3fcf08a9636fd5d181f
redstonerti <redstonerti@gmail.com>
2025-05-09 19:17:33 +0300

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+//AM:
+//email:
+//onoma:
+//epitheto:
+
+//AM:
+//email:
+//onoma:
+//epitheto:
+
+//AM: 3240244
+//email: p3240244@aueb.gr
+//onoma: GEORGIOS
+//epitheto: FITIKIDES
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
