e536a75b5837ea782f0772b67b384cf5d263c599
Biligo <basilisgrigo7@gmail.com>
2025-05-09 20:58:48 +0300

    commit

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@
 //onoma:
 //epitheto:
 
-//AM:
-//email:
-//onoma:
-//epitheto:
+//AM: 3240033
+//email: p3240033@aueb.gr
+//onoma: VASILEIOS
+//epitheto: GRIGOROPOULOS
 
 //AM: 3240244
 //email: p3240244@aueb.gr
