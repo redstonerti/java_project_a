483626e14bab08a8536453c6f3c31174051f2cbb
redstonerti <redstonerti@gmail.com>
2025-05-19 12:29:35 +0300

    DAY 6 (a)

--- BasicReview.java
+++ BasicReview.java
@@ -1,13 +1,12 @@
 public class BasicReview extends Review {
+
     public BasicReview(User user, int rating, String comment, Movie movie) {
         super(user, rating, comment, movie);
+        this.weight = 1;
     }
 
     public BasicReview(User user, int rating, Movie movie) {
         super(user, rating, movie);
-    }
-
-    public int getWeightedRating() {
-        return this.getRating();
+        this.weight = 1;
     }
 }
--- VerifiedReview.java
+++ VerifiedReview.java
@@ -1,13 +1,11 @@
 public class VerifiedReview extends Review {
     public VerifiedReview(VerifiedUser verifiedUser, int rating, Movie movie) {
         super((User) verifiedUser, rating, movie);
+        this.weight = 1.3;
     }
 
     public VerifiedReview(User user, int rating, String comment, Movie movie) {
         super(user, rating, comment, movie);
-    }
-
-    public int getWeightedRating() {
-        return (int) Math.round((float) this.getRating() * 1.3);
+        this.weight = 1.3;
     }
 }
